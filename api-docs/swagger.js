c84a00a988b991d42138114c362a586f42d3c8ba
BetaGun03 <jaimehedrera25@gmail.com>
2025-04-17 23:31:06 +0200

    Cambio en el custom CSS

--- api-docs/swagger.js
+++ api-docs/swagger.js
@@ -46,6 +46,7 @@ const swaggerDocs = swaggerJSDoc(swaggerOptions)
 
 const swaggerUiOptions = {
     explorer: true,
+    customCss:'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
     customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css'
 }
 
