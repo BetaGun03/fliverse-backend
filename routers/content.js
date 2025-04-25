2995dbcfd815072cb56a91fe89bc0d101ce5064f
BetaGun03 <jaimehedrera25@gmail.com>
2025-04-25 09:46:04 +0200

    Se han añadido nuevos endpoints para Content

--- routers/content.js
+++ routers/content.js
@@ -35,6 +35,96 @@ router.post("/contents", auth, upload.single("poster"), async (req, res) => {
     }
 })
 
+router.patch("/contents/:id", auth, upload.single("poster"), async (req, res) => {
+    try{
+        const allowedUpdates =  Object.keys(Content.getAttributes()).filter(attr => !["type", "creation_date"].includes(attr))
+        const updates = Object.keys(req.body)
+
+        // Check if the id is valid
+        if(!req.params.id)
+        {
+            return res.status(400).send("Missing id")
+        }
+
+        const id = parseInt(req.params.id)
+        if(isNaN(id)) 
+        {
+            return res.status(400).send("Invalid id")
+        }
+
+        const content = await Content.findByPk(id)
+
+        if (!content) 
+        {
+            return res.status(404).send("No content found")
+        }
+
+        // Check if the updates are valid
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+        if (!isValidOperation)
+        {
+            return res.status(400).send("Invalid updates")
+        }
+
+        updates.forEach((field) => {
+            content[field] = req.body[field]
+        })
+
+        if(req.file)
+        {
+            content.poster = req.file.buffer
+            content.poster_mime = req.file.mimetype
+        }
+
+        await content.save()
+        res.status(200).send(content)
+    }
+    catch (e) {
+        if(e.name)
+        {
+            if(e.name === "SequelizeUniqueConstraintError")
+            {
+                return res.status(409).send("Content title already in use")
+            }
+            else if(e.name === "SequelizeValidationError")
+            {
+                return res.status(400).send("Invalid data")
+            }
+        }
+        console.error(e)
+        res.status(500).send({ e: "Internal server error" })
+    }
+})
+
+router.get("/contents/searchById", auth, async (req, res) => {
+    try{
+        if(!req.query.id)
+        {
+            return res.status(400).send("Missing id")
+        }
+
+        const id = parseInt(req.query.id)
+        if (isNaN(id)) 
+        {
+           return res.status(400).send("Invalid id")
+        }
+
+        const content = await Content.findByPk(id)
+
+        if (!content) 
+        {
+            return res.status(404).send("No content found")
+        }
+
+        res.status(200).send(content)
+    }
+    catch (e) {
+        console.error(e)
+        res.status(500).send({ e: "Internal server error" })
+    }
+})
+
 router.get("/contents/searchByTitle", auth, async (req, res) => {
     try{
         if(!req.query.title)
@@ -62,6 +152,35 @@ router.get("/contents/searchByTitle", auth, async (req, res) => {
         }
 
         res.status(200).send(contents)
+    }
+    catch (e) {
+        console.error(e)
+        res.status(500).send({ e: "Internal server error" })
+    }
+})
+
+router.get("/contents/posterById", auth, async (req, res) => {
+    try{
+        if(!req.query.id)
+        {
+            return res.status(400).send("Missing id")
+        }
+
+        const id = parseInt(req.query.id)
+        if (isNaN(id)) 
+        {
+           return res.status(400).send("Invalid id")
+        }
+
+        const content = await Content.findByPk(id)
+
+        if (!content) 
+        {
+            return res.status(404).send("No content found")
+        }
+
+        res.set('Content-Type', content.poster_mime)
+        res.status(200).send(content.poster)
     }
     catch (e) {
         console.error(e)
